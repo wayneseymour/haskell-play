f9478b5313d63175cba41e28463109b2dd258509
Tre' Seymour <wayne.seymour@elastic.co>
2021-02-20 19:54:29 -0700

    A little more idiomatic.

--- list-comprehensions.hs
+++ list-comprehensions.hs
@@ -5,15 +5,15 @@
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
 -- Only show elements that are not 13, 15, or 19
-not131519 = [ x | x <- [10..20], x /= 13, x /=15, x /= 19] -- Preloaded with a list
-not131519' xs = [ x | x <- xs, x /= 13, x /=15, x /= 19] -- Takes a list arg
+not131519 xs = [ x | x <- xs, x /= 13, x /=15, x /= 19] -- Preloaded with a list
+not131519' = [10..20] -- Takes a list arg
 
 nouns = ["hobo", "frog", "pope"]
 adjectives = ["lazy", "grouchy", "scheming"]
 
 -- Concat adj " " noun, derived from two lists
-zipWords = [adjective ++ " " ++ noun | adjective <- adjectives, noun <- nouns] -- Preloaded
-zipWords' xs ys = [adjective ++ " " ++ noun | adjective <- xs, noun <- ys] -- Takes a list arg
+zipWords xs ys = [adjective ++ " " ++ noun | adjective <- xs, noun <- ys] -- Takes a list arg
+zipWords' = zipWords adjectives nouns -- Preloaded
 
 -- Replace every element in a list with the number 1,
 -- then add them all up with `sum`
